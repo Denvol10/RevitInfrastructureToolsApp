928e1996b6332f7f79e119632a1c06081a536b6f
Денис Щербатюк <denvol100@gmail.com>
2023-06-05 16:50:54 +0300

    Add project files.

---
+++ RevitInfrastructureTools/App.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace RevitInfrastructureTools
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class App : IExternalApplication
+    {
+        public static string assemblyPath;
+        public static string assemblyFolder;
+        public static string ribbonPath;
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            assemblyPath = typeof(App).Assembly.Location;
+            assemblyFolder = Path.GetDirectoryName(assemblyPath);
+            ribbonPath = assemblyFolder;
+            string tabName = "Revit Infrastructure Tools";
+
+
+            application.CreateRibbonTab(tabName);
+
+            try
+            {
+                CreateDWGtoRVTRibbon(application, tabName);
+                CreateBridgeDeckRibbon(application, tabName);
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Ribbon Sample", ex.ToString());
+
+                return Result.Failed;
+            }
+
+    }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        private void CreateDWGtoRVTRibbon(UIControlledApplication uiapp, string tabName)
+        {
+            string str = "DWG to RVT";
+            RibbonPanel ribbonPanel = uiapp.CreateRibbonPanel(tabName, str);
+            ribbonPanel.AddItem(CreateButtonData("DWGtoRVTLineConverter", "RevitCommand"));
+        }
+
+        private void CreateBridgeDeckRibbon(UIControlledApplication uiapp, string tabName)
+        {
+            string str = "Bridge Deck";
+            RibbonPanel ribbonPanel = uiapp.CreateRibbonPanel(tabName, str);
+            ribbonPanel.AddItem(CreateButtonData("BridgeDeck", "RevitCommand"));
+        }
+
+        public PushButtonData CreateButtonData(string assemblyName, string className)
+        {
+            string dllPath = Path.Combine(ribbonPath, assemblyName + ".dll");
+            string fullClassname = assemblyName + "." + "Infrastructure" + "." + className;
+            string dataPath = Path.Combine(ribbonPath, assemblyName + "_data");
+            string largeIcon = Path.Combine(dataPath, className + "_large.png");
+            string smallIcon = Path.Combine(dataPath, className + "_small.png");
+            string textPath = Path.Combine(dataPath, className + ".txt");
+            string[] text = File.ReadAllLines(textPath);
+            string title = text[0];
+            string tooltip = text[1];
+            string url = text[2];
+
+            PushButtonData data = new PushButtonData(fullClassname, title, dllPath, fullClassname);
+
+            data.LargeImage = new BitmapImage(new Uri(largeIcon, UriKind.Absolute));
+            data.Image = new BitmapImage(new Uri(smallIcon, UriKind.Absolute));
+
+            data.ToolTip = text[1];
+
+            ContextualHelp chelp = new ContextualHelp(ContextualHelpType.Url, url);
+            data.SetContextualHelp(chelp);
+
+            return data;
+        }
+    }
+}
