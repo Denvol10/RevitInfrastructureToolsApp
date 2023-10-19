3bff3844a5f3c2d6551346e20d6df02e4c26bad5
Денис Щербатюк <denvol100@gmail.com>
2023-10-19 16:51:02 +0300

    Add side walk slab button

--- RevitInfrastructureTools/App.cs
+++ RevitInfrastructureTools/App.cs
@@ -108,11 +108,13 @@ namespace RevitInfrastructureTools
             PushButtonData buttonData1 = CreateButtonData("CreatePrefabricatedBeams", "RevitCommand");
             PushButtonData buttonData2 = CreateButtonData("GetBeamTopLines", "RevitCommand");
             PushButtonData buttonData3 = CreateButtonData("SlabOnGradient", "RevitCommand");
+            PushButtonData buttonData4 = CreateButtonData("SideWalkSlab", "RevitCommand");
 
             ribbonPanel.AddItem(buttonData1);
             ribbonPanel.AddItem(buttonData2);
             ribbonPanel.AddSeparator();
             ribbonPanel.AddItem(buttonData3);
+            ribbonPanel.AddItem(buttonData4);
         }
 
         public PushButtonData CreateButtonData(string assemblyName, string className)
